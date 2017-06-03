46204c1a052de19da9204d3e52b22f4fbb57fece
FireWtap <massafra32@gmail.com>
2017-06-03 11:31:49 +0200

    Update base.py

--- base.py
+++ base.py
@@ -17,3 +17,12 @@ def sendTypingAction(chatid):
 
 def sendFile(chatid, file, content):
   return requests.post(Discord + "channels/" + str(chatid) + "/messages", headers={"Authorization":token, "content":str(content)}, files={"file":open(file, 'rb')}).text
+
+def setStatus(status):
+  return requests.post(Discord + "@me/settings", headers={"Authorization":token, "status":str(status)}).text
+
+def pinMessage(chatid, messageid):
+  return requests.post(Discord + "channels/" + str(chatid) + "/pins/" + str(messageid), headers={"Authorization":token}).text
+
+def setName(newname, password, email):
+  return requests.post(Discord + "@me/settings", headers={"username":newname, "password":password, "email":email, "Authorization":token}).text
